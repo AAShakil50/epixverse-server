dbb80c0ffc8911da34cd1a9c1b5c7bac485db3ed
AAShakil50 <aashakil50@gmail.com>
2025-04-04 02:13:15 -0400

    feat: Add type inference for projects and books in schema

--- services/schema.ts
+++ services/schema.ts
@@ -7,13 +7,18 @@ export const projects = pgTable("projects", {
   createdAt: timestamp("created_at").defaultNow().notNull(),
 });
 
-export const books = pgTable(
-  "books",
-  {
-    id: uuid().defaultRandom().primaryKey(),
-    title: varchar().notNull(),
-    description: varchar().notNull(),
-    projectId: uuid("project_id").notNull().references(() => projects.id, { onDelete: "cascade" }),
-    createdAt: timestamp("created_at").defaultNow().notNull(),
-  }
-);
+export const books = pgTable("books", {
+  id: uuid().defaultRandom().primaryKey(),
+  title: varchar().notNull(),
+  description: varchar().notNull(),
+  projectId: uuid("project_id")
+    .notNull()
+    .references(() => projects.id, { onDelete: "cascade" }),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
+
+export type PROJECT = typeof projects.$inferSelect;
+export type NEW_PROJECT = typeof projects.$inferInsert;
+
+export type BOOK = typeof books.$inferSelect;
+export type NEW_BOOK = typeof books.$inferInsert;
